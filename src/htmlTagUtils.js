71b696d031da43e711ba6c2bd5fea35b3a45e8cb
RafGru <rafal_gruca@tlen.pl>
2025-03-05 14:28:37 +0100

    Added two picking tags function

---
+++ src/htmlTagUtils.js
@@ -0,0 +1,16 @@
+// To get one tag
+
+export const oneTagPicker = (tag) => {
+    const pickedTag = document.querySelector(tag)
+    return pickedTag
+}
+
+// To get tag collection
+
+export const tagCollectionPicker = (tags) => {
+    const tagCollection = document.querySelectorAll(tags)
+    return tagCollection 
+}
+
+// To manipulate tags 
+
