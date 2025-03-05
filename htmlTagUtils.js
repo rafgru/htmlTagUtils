741f3f192894a9c9b5d9584a16074dece1010eae
Rafał Gruca <rafal_gruca@tlen.pl>
2025-03-05 17:44:58 +0100

    update tag class list function added

---
+++ htmlTagUtils.js
@@ -0,0 +1,37 @@
+// To get one tag
+
+export const oneTagPicker = (tag) => {
+    const pickedTag = document.querySelector(tag)
+    return pickedTag
+}
+
+// To get tag collection
+
+export const tagCollectionPicker = (tags) => {
+    const tagCollection = document.querySelectorAll(tags)
+    return tagCollection 
+}
+
+// To update tag class list
+
+updateClassList (element, action, className) {
+    if (!element) {
+        console.error('Element nie istnieje');
+        return;
+    }
+
+    switch(action) {
+        case 'add':
+            element.classList.add(className);
+            break;
+        case 'remove':
+            element.classList.remove(className);
+            break;
+        case 'toggle':
+            element.classList.toggle(className);
+            break;
+        default:
+            console.error(`Nieznana akcja: ${action}`);
+    }
+};
+
